49536bd1bdeafc3450ebda7b91313b96c80b48e4
1h3ndy <157691781+1h3ndy@users.noreply.github.com>
2024-04-22 08:45:35 +0000

    loginv1

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
-
+    user =request.args.get("name")
+    print(user)
+    if user == None:
+    
+        return render_template("index.html")
+    elif user == "admin":
+        return "Hello admin"
+    else: 
+        return "hello " + user
 app.run(host="0.0.0.0",debug=True)
