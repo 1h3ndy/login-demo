b66c21d57edf778453f16e9f03a79da4872116a4
1h3ndy <157691781+1h3ndy@users.noreply.github.com>
2024-04-25 12:57:27 +0000

    not working and too far ahead

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+login = {}
 @app.route("/", methods=["POST","GET"])
 def hello_world():
 
@@ -15,12 +15,28 @@ def hello_world():
         user = request.form["un"]
         password = request.form["pw"]
         print(user)
-        if user == "admin" and password == "monkeys":
-            return "Hello Admin"
+
+        if user in login:
+            if login[user]['password'] == password:
+                return "Hello " + user
+            else:
+                return "Invalid Password"
+
         else:
-            return "Hello " + user
-        
+            return "Invalid Username"
 
+@app.route("/register", methods=["POST","GET"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
 
+    if request.method == "POST":
+        user = request.form["un"]
+        password = request.form["pw"]
+        login[user]['username'] = user
+        login[user]['password'] = password
+        return render_template("index.html")
+
+ 
 
 app.run(host="0.0.0.0",debug=True)
