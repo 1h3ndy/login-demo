c7a02000723eac75500c1a3b2a5aab2eccdae2bf
1h3ndy <157691781+1h3ndy@users.noreply.github.com>
2024-04-25 11:39:39 +0000

    login v2

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    user =request.args.get("name")
+    user =request.args.get("un")
+    password =request.args.get("pw")
     print(user)
     if user == None:
     
         return render_template("index.html")
-    elif user == "admin":
-        return "Hello admin"
+    elif user == "admin" and password == "monkeys":
+        return "Hello to the admin"
+    
     else: 
         return "hello " + user
 app.run(host="0.0.0.0",debug=True)
