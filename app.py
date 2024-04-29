7b3309f7b70bb18019472cf0189f0a84b71fe46d
1h3ndy <157691781+1h3ndy@users.noreply.github.com>
2024-04-29 08:57:17 +0000

    v4

--- app.py
+++ app.py
@@ -3,27 +3,32 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 login = {}
-@app.route("/", methods=["POST","GET"])
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-
-        
-    if request.method == "GET":
-
-        return render_template("index.html")
-
-    else:
-        user = request.form["un"]
-        password = request.form["pw"]
+    
+    if request.method == 'POST':
+        print(request)
+        user = request.form.get('username')
         print(user)
-
-        if user in login:
-            if login[user]['password'] == password:
-                return "Hello " + user
+        password = request.form.get('password')
+        print(password)
+        f = open('login.txt', 'r')
+        
+        for line in f.readlines():
+            
+            line.strip()
+            file_user, file_password = line.split(':')
+            if file_user == user and file_password == password:
+                return 'Login successful'
             else:
-                return "Invalid Password"
+                return 'Invalid usernam'
+    else:
+            return render_template('index.html')
+    
 
-        else:
-            return "Invalid Username"
 
 @app.route("/register", methods=["POST","GET"])
 def register():
@@ -33,8 +38,9 @@ def register():
     if request.method == "POST":
         user = request.form["un"]
         password = request.form["pw"]
-        login[user]['username'] = user
-        login[user]['password'] = password
+        f = open("login.txt", "a")
+        f.write(f"\n{user}:{password}")
+        f.close()
         return render_template("index.html")
 
  
